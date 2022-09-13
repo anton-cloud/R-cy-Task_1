c613baf570dc1b0ce04d14fe4d3c135a436dc1ba
Anton Sharudylo <a.sharudylo@gmail.com>
2022-09-13 22:23:11 +0300

    without_archive_wiew

---
+++ data/notes.js
@@ -0,0 +1,22 @@
+export const notes = [{
+    id: 1,
+    name: "Shopping list",
+    created: "Apil 20, 2020",
+    category: 'task',
+    content: 'Lorem, ipsum.',
+    dates: "3/5/2020, 3/5/2020",
+    active: true,
+    icon: "fa-list-check"
+},
+{
+    id: 2,
+    name: "Shopping list",
+    created: "Apil 20, 2020",
+    category: 'task',
+    content: 'Lorem, ipsum.',
+    dates: "3/5/2020, 3/5/2020",
+    active: false,
+    icon: "fa-list-check"
+}
+]
+
---
+++ js/sctipt.js
@@ -0,0 +1,212 @@
+import { notes as initialNotes } from "../data/notes.js";
+import { notesArchive as initialNotesArchive } from "../data/notesArchive.js";
+
+const notes = [...initialNotes]
+const notesArchive = [...initialNotesArchive]
+const icons = { task: "fa-list-check", idea: "fa-lightbulb", random_thought: "fa-star" }
+
+
+function updateArchiveData() {
+
+  notesArchive[0].active = getActiveCountTask(notes)
+  notesArchive[0].archived = getArchivedCountTask(notes)
+
+  notesArchive[1].active = getActiveCountRandomThought(notes)
+  notesArchive[1].archived = geArchivedCountRandomThought(notes)
+
+  notesArchive[2].active = getActiveCountIdea(notes)
+  notesArchive[2].archived = getArchivedCountIdea(notes)
+}
+
+let editModalOpen = false;
+let changeNodeId = null;
+
+// all eventlisteners
+const notesList = document.querySelector(".notes-table__list");
+const notesArchiveList = document.querySelector(".notes-archive-table__list");
+const createNoteBtn = document.querySelector(".create-node")
+const modal = document.querySelector(".modal")
+const modalContent = document.querySelector('.modal__content')
+const form = document.getElementById("form");
+
+notesList.addEventListener('click', deleteNote);
+notesList.addEventListener('click', editNote);
+notesList.addEventListener('click', archiveNote);
+
+createNoteBtn.addEventListener('click', () => { modal.classList.add("active") })
+modal.addEventListener("click", () => { modal.classList.remove("active"), changeNodeId = null, form.reset() })
+modalContent.addEventListener('click', (e) => { e.stopPropagation() })
+form.addEventListener('submit', handleSubmit)
+
+function getActiveCountTask(notes) {
+  const activeTaskArr = notes.filter((note) => note.category === "task" && note.active)
+  return activeTaskArr.length;
+}
+
+function getArchivedCountTask(notes) {
+  const archivedTaskArr = notes.filter((note) => note.category === "task" && !note.active)
+  return archivedTaskArr.length;
+}
+
+function getActiveCountRandomThought(notes) {
+  const activeRandomThoughtArr = notes.filter((note) => note.category === "random_thought" && note.active)
+  return activeRandomThoughtArr.length;
+}
+
+function geArchivedCountRandomThought(notes) {
+  const archivedRandomThoughtArr = notes.filter((note) => note.category === "random_thought" && !note.active)
+  return archivedRandomThoughtArr.length;
+}
+
+function getActiveCountIdea(notes) {
+  const activeIdeaArr = notes.filter((note) => note.category === "idea" && note.active)
+  return activeIdeaArr.length;
+}
+
+function getArchivedCountIdea(notes) {
+  const archivedIdeaArr = notes.filter((note) => note.category === "idea" && !note.active)
+  return archivedIdeaArr.length;
+}
+
+function renderNotesTable() {
+  console.log("render renderNotesTable");
+  notesList.innerHTML = notes.map((note) => {
+    if (note.active) {
+      return ` <tr data-id=${note.id}>
+    <td class="first-col">
+    <div><i class="fa-sharp fa-solid ${note.icon}"></i></div>
+    ${note.name}
+    </td>
+    <td>${note.created}</td>
+    <td>${note.category}</td>
+    <td>${note.content}</td>
+    <td>${note.dates}</td>
+    <td>
+    <button class="btn edit-note-btn">
+    <i class="fa-sharp fa-solid fa-pencil"></i>
+    </button>
+    <button class="btn archive-note-btn">
+    <i class="fa-sharp fa-solid fa-download"></i>
+    </button>
+    <button class="btn delete-note-btn">
+    <i class="fa-sharp fa-solid fa-trash"></i>
+    </button>
+    </td>
+    </tr >`}
+  }).join('')
+}
+
+function renderArchiveTable() {
+  console.log("render renderArchiveTable");
+  notesArchiveList.innerHTML = notesArchive.map((note) => {
+    return ` <tr>
+  <td class="first-col">
+  <div><i class="fa-sharp fa-solid ${note.icon}"></i></div>
+  ${note.category}
+  </td >
+  <td >${note.active}</td>
+  <td>${note.archived}</td>
+  </tr> `
+  }).join("")
+}
+
+renderNotesTable()
+updateArchiveData()
+renderArchiveTable()
+
+// archive a note 
+function archiveNote(e) {
+  if (e.target.classList.contains('archive-note-btn')) {
+    console.log('click');
+    const currentNodeId = e.target.parentElement.parentElement.dataset.id;
+    const index = notes.findIndex((note) => note.id === Number(currentNodeId))
+    console.log(index);
+    notes[index].active = !notes[index].active
+
+    renderNotesTable()
+    updateArchiveData()
+    renderArchiveTable()
+  }
+}
+// delete a note 
+function deleteNote(e) {
+  if (e.target.classList.contains('delete-note-btn')) {
+    const currentNodeId = e.target.parentElement.parentElement.dataset.id;
+    const index = notes.findIndex((note) => note.id === Number(currentNodeId))
+    notes.splice(index, 1);
+    e.target.parentElement.parentElement.remove(); // removing from DOM
+    console.log(notes);
+    renderNotesTable()
+    renderArchiveTable()
+  }
+}
+
+// edit a note 
+function editNote(e) {
+  if (e.target.classList.contains('edit-note-btn')) {
+    editModalOpen = !editModalOpen;
+
+    const currentId = e.target.parentElement.parentElement.dataset.id;
+    console.log(currentId);
+    const currentNote = notes.find((note) => note.id.toString() === currentId.toString())
+    const formName = document.querySelector('[name="name"]');
+    const formCategory = document.querySelector('[name="category"]');
+    const formContent = document.querySelector('[name="content"]');
+
+    changeNodeId = Number(currentId);
+    console.log(changeNodeId);
+    modal.classList.add("active")
+
+    formName.value = currentNote?.name;
+    formCategory.selected = currentNote?.category;
+    formContent.value = currentNote?.content;
+    // formButton.textContent = "change";
+    console.log(notes);
+    renderNotesTable()
+    updateArchiveData()
+    renderArchiveTable()
+  }
+}
+
+// submit 
+function handleSubmit(e) {
+  event.preventDefault();
+
+  const {
+    elements: { name, category, content }
+  } = event.currentTarget;
+
+  if (name.value === "" || category.value === "" || content.value === "") {
+    return;
+  }
+
+  console.log(category.value);
+  const newNote = {
+    id: changeNodeId ? changeNodeId : Date.now(),
+    name: name.value,
+    created: Date.now(),
+    category: category.value,
+    content: content.value,
+    dates: null,
+    active: true,
+    icon: icons[category.value],
+  }
+
+  modal.classList.remove("active")
+
+  if (changeNodeId) {
+    let changeableNoteIdx = notes.findIndex((note) => note.id === changeNodeId)
+    notes[changeableNoteIdx] = { ...notes[changeableNoteIdx], ...newNote }
+  } else {
+    notes.push(newNote)
+  }
+
+
+  renderNotesTable()
+  updateArchiveData()
+  renderArchiveTable()
+
+  changeNodeId = null;
+  form.reset();
+  console.log(notes);
+}
